bdeb4637a1afc77213738c506eddeaf3b2ff8ce3
Joan Goyeau <joan@goyeau.com>
2017-12-07 23:09:04 +0000

    Support for PodDisruptionBudget

--- src/main/scala/com/goyeau/kubernetesclient/KubernetesClient.scala
+++ src/main/scala/com/goyeau/kubernetesclient/KubernetesClient.scala
@@ -14,4 +14,5 @@ case class KubernetesClient(config: KubeConfig)(implicit system: ActorSystem) {
   lazy val configMaps = ConfigMapsOperations(config)
   lazy val secrets = SecretsOperations(config)
   lazy val horizontalPodAutoscalers = HorizontalPodAutoscalersOperations(config)
+  lazy val podDisruptionBudgets = PodDisruptionBudgetsOperations(config)
 }
