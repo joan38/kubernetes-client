20b91c55e95593f481454d54a7751fa0c024f534
Alexey Novakov <novakov.alex@gmail.com>
2020-04-13 20:47:39 +0200

    Upgrade to k8s v1.18

--- project/Definition.scala
+++ project/Definition.scala
@@ -2,13 +2,21 @@ case class Definition(
     description: Option[String],
     required: Option[Seq[String]],
     properties: Option[Map[String, Property]],
-    `type`: Option[String]
+    `type`: Option[String],
+    `x-kubernetes-group-version-kind`: Option[Seq[Kind]]
 )
 
 case class Property(
     description: Option[String],
+    format: Option[String],
     `type`: Option[String],
     items: Option[Property],
     additionalProperties: Option[Property],
     $ref: Option[String]
+)
+
+case class Kind(
+    group: String,
+    kind: String,
+    version: String
 )
