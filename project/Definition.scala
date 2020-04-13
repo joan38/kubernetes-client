ab4caee6ed4b36279b83656b59e07e30a1c78796
Alexey Novakov <novakov.alex@gmail.com>
2020-04-13 13:56:43 +0200

    fortmat code

--- project/Definition.scala
+++ project/Definition.scala
@@ -3,20 +3,20 @@ case class Definition(
     required: Option[Seq[String]],
     properties: Option[Map[String, Property]],
     `type`: Option[String],
-  `x-kubernetes-group-version-kind`: Option[Seq[Kind]],
+    `x-kubernetes-group-version-kind`: Option[Seq[Kind]]
 )
 
 case class Property(
     description: Option[String],
     format: Option[String],
-  `type`: Option[String],
-  items: Option[Property],
-  additionalProperties: Option[Property],
-  $ref: Option[String]
+    `type`: Option[String],
+    items: Option[Property],
+    additionalProperties: Option[Property],
+    $ref: Option[String]
 )
 
 case class Kind(
-  group: String,
-  kind: String,
-  version: String
+    group: String,
+    kind: String,
+    version: String
 )
