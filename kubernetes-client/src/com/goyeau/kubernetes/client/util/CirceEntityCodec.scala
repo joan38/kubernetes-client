ba1f8a282237ca137305ea456b8cdaca039a1ade
Alexey Novakov <novakov.alex@gmail.com>
2021-06-24 18:46:24 +0200

    remove unused type class instance

--- kubernetes-client/src/com/goyeau/kubernetes/client/util/CirceEntityCodec.scala
+++ kubernetes-client/src/com/goyeau/kubernetes/client/util/CirceEntityCodec.scala
@@ -1,6 +1,5 @@
 package com.goyeau.kubernetes.client.util
 
-import cats.Applicative
 import cats.effect.Sync
 import io.circe.{Decoder, Encoder, Printer}
 import org.http4s.{EntityDecoder, EntityEncoder}
@@ -9,6 +8,6 @@ import org.http4s.circe.CirceInstances
 private[client] object CirceEntityCodec extends CirceInstances {
   override val defaultPrinter: Printer = Printer.noSpaces.copy(dropNullValues = true)
 
-  implicit def circeEntityEncoder[F[_]: Applicative, A: Encoder]: EntityEncoder[F, A] = jsonEncoderOf[F, A]
-  implicit def circeEntityDecoder[F[_]: Sync, A: Decoder]: EntityDecoder[F, A]        = jsonOf[F, A]
+  implicit def circeEntityEncoder[F[_], A: Encoder]: EntityEncoder[F, A]       = jsonEncoderOf[F, A]
+  implicit def circeEntityDecoder[F[_]: Sync, A: Decoder]: EntityDecoder[F, A] = jsonOf[F, A]
 }
